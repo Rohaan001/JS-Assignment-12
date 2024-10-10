cae6dfc1531b263bb914bd62bf71c2d5e3afcdcc
Zeeshan Islam <159948421+Rohaan001@users.noreply.github.com>
2024-10-10 23:10:17 +0500

    add

---
+++ app.js
@@ -0,0 +1,44 @@
+//Task 01
+
+// var select1 = prompt("Enter your first Choose (1 se 2)");
+// var select2 = prompt("Enter your second Choose (1 se 2)");
+
+// var randomIndex = Math.floor(Math.random() * 5);
+
+// if (randomIndex === 0) {
+//     console.log("Head" );
+// } else {
+//     console.log("Tail" );
+// }
+
+//Task 02
+
+
+// var str = +prompt("enter a dice number from (1 to 6)")
+
+// var randomIndex = Math.floor(Math.random() * 7);
+// console.log("its your num chechk choose its real "+randomIndex);
+
+// if (str === randomIndex) {
+// console.log("wins");
+// } else {
+// console.log("com wins");
+// }
+// var str = "012345678910abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@##$%^&*()_+" 
+// var str = Math.floor(Math.random()*110000000)
+
+
+
+
+
+// var pass1 = prompt("enter your name")
+// var randomPass = Math.floor(Math.random()*110000000)
+// var fixed  = randomPass.toFixed(10)
+// alert(randomPass)
+
+
+
+
+
+
+
